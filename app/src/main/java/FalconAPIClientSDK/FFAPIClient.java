a4e6feed8e14668bdda7d983f780599cab83e91d
luisresende <luisfrvilela@gmail.com>
2016-05-05 11:23:41 -0300

    Implements FFAPIClient class

--- app/src/main/java/FalconAPIClientSDK/FFAPIClient.java
+++ app/src/main/java/FalconAPIClientSDK/FFAPIClient.java
@@ -4,4 +4,48 @@ package FalconAPIClientSDK;
  * Created by luisresende on 04/05/16.
  */
 public class FFAPIClient {
+
+    private  String apiBaseUrl="";
+    private  String apiKey = "";
+
+    private static FFAPIClient ourInstance = new FFAPIClient();
+
+    /**
+     * Returns an Singleton Instance
+     *
+     * @return  the static instance of this class
+     */
+    public static FFAPIClient sharedClient() {
+        return ourInstance;
+    }
+
+    /**
+     * Returns an Singleton Instance with parametres url and apiKey
+     *
+     * @param url an api base url for client
+     * @param key an api key of client in server
+     * @return  the static instance of this class
+     */
+    public FFAPIClient initWithBaseUrlAndApiKey(String url, String key){
+        sharedClient().apiBaseUrl = url;
+        sharedClient().apiKey = key;
+        return sharedClient();
+    }
+
+    public String getApiBaseUrl() {
+        return apiBaseUrl;
+    }
+
+    public void setApiBaseUrl(String apiBaseUrl) {
+        this.apiBaseUrl = apiBaseUrl;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
 }
