2563489db834bc4b911cf539661f4ef412b21ddd
luisresende <luisfrvilela@gmail.com>
2016-05-05 10:24:55 -0300

    Create Interface FFObjectResponse

--- app/src/main/java/FalconAPIClientSDK/FFError.java
+++ app/src/main/java/FalconAPIClientSDK/FFError.java
@@ -4,4 +4,5 @@ package FalconAPIClientSDK;
  * Created by luisresende on 05/05/16.
  */
 public class FFError {
+
 }
--- app/src/main/java/FalconAPIClientSDK/FFObjectResponse.java
+++ app/src/main/java/FalconAPIClientSDK/FFObjectResponse.java
@@ -1,7 +1,15 @@
 package FalconAPIClientSDK;
 
+import java.util.ArrayList;
+
 /**
  * Created by luisresende on 05/05/16.
  */
 public interface FFObjectResponse {
+    void afterFindSuccess(ArrayList<Object> objects);
+    void afterSaveSuccess(Object object);
+    void afterDeleteSuccess(String status);
+    void afterFindError(FFError error);
+    void afterSaveError(FFError error);
+    void afterDeleteError(FFError error);
 }
