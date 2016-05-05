90dd98f9c27864bf362490daeaf884e511d93158
luisresende <luisfrvilela@gmail.com>
2016-05-05 11:09:43 -0300

    Document Interface FFObjectResponse

--- app/src/main/java/FalconAPIClientSDK/FFObjectResponse.java
+++ app/src/main/java/FalconAPIClientSDK/FFObjectResponse.java
@@ -6,10 +6,52 @@ import java.util.ArrayList;
  * Created by luisresende on 05/05/16.
  */
 public interface FFObjectResponse {
+
+    /**
+     * This method is for running a code after operation
+     * find become success
+     *
+     * @param objects list of return objects if find success
+     */
     void afterFindSuccess(ArrayList<Object> objects);
+
+    /**
+     * This method is for running a code after operation
+     * save become success
+     *
+     * @param object a return object if save success
+     */
     void afterSaveSuccess(Object object);
+
+    /**
+     * This method is for running a code after operation
+     * delete become success
+     *
+     * @param status an actual status if delete success
+     */
     void afterDeleteSuccess(String status);
+
+    /**
+     * This method is for running a code after operation
+     * find return an error.
+     *
+     * @param error an error that causes find operation fail
+     */
     void afterFindError(FFError error);
+
+    /**
+     * This method is for running a code after operation
+     * save return an error.
+     *
+     * @param error an error that causes save operation fail
+     */
     void afterSaveError(FFError error);
+
+    /**
+     * This method is for running a code after operation
+     * delete return an error.
+     *
+     * @param error an error that causes delete operation fail
+     */
     void afterDeleteError(FFError error);
 }
