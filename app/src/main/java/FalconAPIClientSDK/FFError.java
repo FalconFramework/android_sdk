d04705be48aa3b1d26c0a79f54a202bcda9205e3
luisresende <luisfrvilela@gmail.com>
2016-05-05 10:37:59 -0300

    Create FFError

--- app/src/main/java/FalconAPIClientSDK/FFError.java
+++ app/src/main/java/FalconAPIClientSDK/FFError.java
@@ -5,4 +5,19 @@ package FalconAPIClientSDK;
  */
 public class FFError {
 
+    private Integer cod=0;
+    private String message="";
+
+    /**
+     * Returns an Error Description Message that can then be show
+     * on the screen.
+     * This method construct an error description message with
+     * his actual propertys
+     *
+     * @return      the error description message
+     */
+    private String FFErrorDescriptionMessage(){
+        return "Error " + cod + ": " + message;
+    }
+
 }
