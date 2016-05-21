328f03b06b151f05e8bb9176c886277f961ebed9
Gabriel Araújo <gabriel@startae.com.br>
2016-05-21 00:31:47 -0300

    Add initial methods for creating URL request

--- app/src/main/java/FalconAPIClientSDK/FFUrlRequest.java
+++ app/src/main/java/FalconAPIClientSDK/FFUrlRequest.java
@@ -1,7 +1,27 @@
 package FalconAPIClientSDK;
+import org.atteo.evo.inflector.English;
 
-/**
- * Created by luisresende on 04/05/16.
- */
+
 public class FFUrlRequest {
+
+    private FFAPIClient apiSettings = FFAPIClient.sharedClient();
+
+
+    private String buildURL(String resourceName) {
+        String baseURL = this.apiSettings.getApiBaseUrl();
+        String pulizeredResourceName = English.plural(resourceName);
+
+        return baseURL + pulizeredResourceName;
+    }
+
+    private String buildURL(String resourceName, int id) {
+        String baseURL = this.apiSettings.getApiBaseUrl();
+        String pulizeredResourceName = English.plural(resourceName);
+
+        return baseURL + pulizeredResourceName + "/" + id;
+    }
+
+    public String getAll(String resourceName) {
+        return this.buildURL(resourceName);
+    }
 }
